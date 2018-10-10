4bb6fdfe44313e50a90e5978fb3469d8e4efb82a
Mbah Clinton <clintonmbah44@gmail.com>
2018-10-10 10:36:25 +0000

    added mask for cvv and expiry date

--- checkout/src/main/java/com/fayapay/checkout/fragments/CardPayFragment.kt
+++ checkout/src/main/java/com/fayapay/checkout/fragments/CardPayFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import com.fayapay.checkout.R
 import com.fayapay.checkout.presenters.CardPayPresenter
 import com.fayapay.checkout.util.CheckoutStage
+import com.redmadrobot.inputmask.MaskedTextChangedListener
 import kotlinx.android.synthetic.main.fragment_card_pay.*
 
 internal class CardPayFragment : CheckoutStage(), CardPayPresenter.View {
@@ -21,6 +22,14 @@ internal class CardPayFragment : CheckoutStage(), CardPayPresenter.View {
 
         continueBtn.setOnClickListener { presenter.checkout() }
         backBtn.setOnClickListener { presenter.navigateBack() }
+
+        val expiryMask = MaskedTextChangedListener("[00]{/}[00]", expiryTb, null)
+        expiryTb.addTextChangedListener(expiryMask)
+        expiryTb.setOnFocusChangeListener(expiryMask)
+
+        val cvvMask = MaskedTextChangedListener("[000]", cvvTb, null)
+        cvvTb.addTextChangedListener(cvvMask)
+        cvvTb.setOnFocusChangeListener(cvvMask)
     }
 
     override fun notifyActionPerformed(action: String) {
